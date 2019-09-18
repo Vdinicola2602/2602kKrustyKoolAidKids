b317bc44dba88d82f5095a276b44dd7daa09ef59
Draconicus Malicalus <firstgcoders@gmail.com>
2019-09-18 16:11:05 -0400

    Minecraft Patch 1

--- 2602kKrustyKoolAidKi/src/main.cpp
+++ 2602kKrustyKoolAidKi/src/main.cpp
@@ -16,8 +16,8 @@ vex::brain       Brain;
 vex::competition Competition;
 
 // define your global instances of motors and other devices here
-
-
+vex::motor BackLeft        = vex::motor( vex::PORT1 );
+vex::motor RightMotor       = vex::motor
 /*---------------------------------------------------------------------------*/
 /*                          Pre-Autonomous Functions                         */
 /*                                                                           */
@@ -63,15 +63,16 @@ void autonomous( void ) {
 
 void usercontrol( void ) {
   // User control code here, inside the loop
+  
   while (1) {
     // This is the main execution loop for the user control program.
     // Each time through the loop your program should update motor + servo 
     // values based on feedback from the joysticks.
-
-    // ........................................................................
+        // ........................................................................
     // Insert user code here. This is where you use the joystick values to 
     // update your motors, etc.
     // ........................................................................
+    
  
     vex::task::sleep(20); //Sleep the task for a short amount of time to prevent wasted resources. 
   }
